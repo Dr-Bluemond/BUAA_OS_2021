041e8395df6486c43cdad06ce8a1ff59823ec304
19231140 <19231140@stu-116.(none)>
2021-03-09 15:34:37 +0800

    lab0 finished

--- csc/code/fibo.c
+++ csc/code/fibo.c
@@ -1,13 +1,13 @@
 #include <fibo.h>
-char fibo(char n)
+int fibo(int n)
 {
-	char a;
-	char b;
-	char c;
+	int a;
+	int b;
+	int c;
 	if(n==1){
 		a=1;
 	}else{
-		char i;
+		int i;
 		c=0;
 		b=1;
 		for(i=2;i<=n;i++){
--- ray/sh_test1/changefile.sh
+++ ray/sh_test1/changefile.sh
@@ -5,9 +5,14 @@ do
 	if [ $a -gt 70 ]	   #if loop variable is greater than 70
 	then
 
+	rm -rf file$a
+
 	elif [ $a -gt 40 ]         # else if loop variable is great than 40   
 	then
+
+	mv file$a newfile$a
 	
 	fi
 	           #don't forget change the loop variable
+	a=$[$a+1]
 done
