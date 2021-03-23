4f001eec8e322831c813634c291b6a5864c8761c
bluemond <dr_bluemond@126.com>
2021-03-23 14:55:45 +0800

    fix fucking bug

--- readelf/readelf.c
+++ readelf/readelf.c
@@ -64,30 +64,36 @@ int readelf(u_char *binary, int size)
 
         // get section table addr, section header number and section header size.
 		if (data == 2) {
-			uint32_t e_shoff = ehdr->e_shoff;
+
 			uint32_t temp = 0;
-				for (i = 0; i < 4; i++) {
-					temp = temp << 8 + e_shoff & 255;
-					e_shoff >>= 8;
-				}
-				e_shoff = temp;
-			Elf32_Shdr *shdr = (Elf32_Shdr *)(binary + e_shoff);
-			Elf32_Half sh_entry_count = ehdr->e_shnum;
 			uint16_t temp16 = 0;
-				for (i = 0; i < 2; i++) {
-					temp16 = temp16 << 8 + sh_entry_count & 255;
-					sh_entry_count >>= 8;
-				}
-				sh_entry_count = temp16;
+
+			uint32_t e_shoff = ehdr->e_shoff;
+			for (i = 0; i < 4; i++) {
+				temp = (temp << 8) + (e_shoff & 255);
+				e_shoff >>= 8;
+			}
+			e_shoff = temp;
+
+			Elf32_Shdr *shdr = (Elf32_Shdr *)(binary + e_shoff);
+
+			uint16_t sh_entry_count = ehdr->e_shnum;
+			for (i = 0; i < 2; i++) {
+				temp16 = (temp16 << 8) + (sh_entry_count & 255);
+				sh_entry_count >>= 8;
+			}
+			sh_entry_count = temp16;
 
 			for (Nr = 0; Nr < sh_entry_count; Nr ++) {
-				Elf32_Addr sh_addr = shdr[Nr].sh_addr;
+				// sh_addr
+				uint32_t sh_addr = shdr[Nr].sh_addr;
 				temp = 0;
 				for (i = 0; i < 4; i++) {
-					temp = temp << 8 + sh_addr & 255;
+					temp = (temp << 8) + (sh_addr & 255);
 					sh_addr >>= 8;
 				}
 				sh_addr = temp;
+				// sh_addr
 				printf("%d:0x%x\n", Nr, sh_addr);
 			}
 			
