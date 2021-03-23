97c088f18b670089c84f34e5f87edbfa6814e506
bluemond <dr_bluemond@126.com>
2021-03-23 14:28:43 +0800

    finished Extra

--- readelf/readelf.c
+++ readelf/readelf.c
@@ -48,12 +48,12 @@ int readelf(u_char *binary, int size)
         Elf32_Ehdr *ehdr = (Elf32_Ehdr *)binary;
 
         int Nr;
+		int i;
 
-        Elf32_Shdr *shdr = (Elf32_Shdr *)(binary + ehdr->e_shoff);
+
+		unsigned char data = ehdr->e_ident[5];
 
         u_char *ptr_sh_table = NULL;
-        Elf32_Half sh_entry_count = ehdr->e_shnum;
-        Elf32_Half sh_entry_size = ehdr->e_shentsize;
 
 
         // check whether `binary` is a ELF file.
@@ -63,8 +63,42 @@ int readelf(u_char *binary, int size)
         }
 
         // get section table addr, section header number and section header size.
-		for (Nr = 0; Nr < sh_entry_count; Nr ++) {
-			printf("%d:0x%x\n", Nr, shdr[Nr].sh_addr);
+		if (data == 2) {
+			uint32_t e_shoff = ehdr->e_shoff;
+			uint32_t temp = 0;
+				for (i = 0; i < 4; i++) {
+					temp = temp << 8 + e_shoff & 255;
+					e_shoff >>= 8;
+				}
+				e_shoff = temp;
+			Elf32_Shdr *shdr = (Elf32_Shdr *)(binary + e_shoff);
+			Elf32_Half sh_entry_count = ehdr->e_shnum;
+			uint16_t temp16 = 0;
+				for (i = 0; i < 2; i++) {
+					temp16 = temp16 << 8 + sh_entry_count & 255;
+					sh_entry_count >>= 8;
+				}
+				sh_entry_count = temp16;
+
+			for (Nr = 0; Nr < sh_entry_count; Nr ++) {
+				Elf32_Addr sh_addr = shdr[Nr].sh_addr;
+				temp = 0;
+				for (i = 0; i < 4; i++) {
+					temp = temp << 8 + sh_addr & 255;
+					sh_addr >>= 8;
+				}
+				sh_addr = temp;
+				printf("%d:0x%x\n", Nr, sh_addr);
+			}
+			
+		} else {
+			Elf32_Phdr *phdr = (Elf32_Phdr *)(binary + ehdr->e_phoff);
+			Elf32_Half ph_entry_count = ehdr->e_phnum;
+
+			for (Nr = 0; Nr < ph_entry_count; Nr ++) {
+				printf("%d:0x%x,0x%x\n", Nr, phdr[Nr].p_filesz , phdr[Nr].p_memsz);
+			}
+
 		}
 
         // for each section header, output section number and section addr. 
@@ -73,4 +107,6 @@ int readelf(u_char *binary, int size)
 
         return 0;
 }
+
+
 
