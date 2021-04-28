ff9cc08ee2c24ddbb1165ffc4ae59aec24179cf4
bluemond <dr_bluemond@126.com>
2021-04-28 22:49:27 +0800

    fuck

--- lib/sched.c
+++ lib/sched.c
@@ -14,37 +14,84 @@
 /*** exercise 3.14 ***/
 void sched_yield(void)
 {
-    static int count = 0; // remaining time slices of current env
-    static int point = 0; // current env_sched_list index
-	static struct Env *e;
-    
-    /*  hint:
-     *  1. if (count==0), insert `e` into `env_sched_list[1-point]`
-     *     using LIST_REMOVE and LIST_INSERT_TAIL.
-     *  2. if (env_sched_list[point] is empty), point = 1 - point;
-     *     then search through `env_sched_list[point]` for a runnable env `e`, 
-     *     and set count = e->env_pri
-     *  3. count--
-     *  4. env_run()
-     *
-     *  functions or macros below may be used (not all):
-     *  LIST_INSERT_TAIL, LIST_REMOVE, LIST_FIRST, LIST_EMPTY
-     */
-	if (count <= 0) {
-		do {
-			if (LIST_EMPTY(&env_sched_list[point])) {
-				point = 1 - point;
-			}
-
-			e = LIST_FIRST(&env_sched_list[point]);
-
-			if (e != NULL) {
-				LIST_REMOVE(e, env_sched_link);
-				LIST_INSERT_TAIL(&env_sched_list[1 - point], e, env_sched_link);
-				count = e->env_pri;
-			}
-		} while (e == NULL || e->env_status != ENV_RUNNABLE);
+	/*
+	static u_int cur_lasttime = 1;
+	struct Env *next_env;
+	cur_lasttime--;
+	if (cur_lasttime == 0 || curenv == NULL)
+	{
+		if (curenv != NULL)
+		{
+			LIST_INSERT_TAIL(&env_sched_list[1], curenv, env_sched_link);
+		}
+		if (!LIST_EMPTY(&env_sched_list[0]))
+		{
+			next_env = LIST_FIRST(&env_sched_list[0]);
+			LIST_REMOVE(next_env, env_sched_link);
+		}
+		else
+		{
+			next_env = LIST_FIRST(&env_sched_list[1]);
+			LIST_REMOVE(next_env, env_sched_link);
+		}
+		cur_lasttime = next_env->env_pri;
+		//printf("%x %x\n",curenv, next_env);
+		env_run(next_env);
 	}
-	count --;
-	env_run(e);
+	env_run(curenv);
+	//printf("%x",*((int*)(TF_EPC + TIMESTACK - TF_SIZE)));
+	//printf("Sched_yield End\n");
+	*/
+	//printf("Sched_yield happen\n");
+	static int cur_lasttime = 1;
+	static int cur_head_index = 0;
+	struct Env *next_env;
+	int now_have = 0;
+	cur_lasttime--;
+	if (cur_lasttime <= 0 || curenv == NULL || curenv->env_status != ENV_RUNNABLE)
+	{
+		now_have = 0;
+		while(1)
+		{
+			if (LIST_EMPTY(&env_sched_list[cur_head_index]))
+			{
+				cur_head_index = !cur_head_index;
+				break;
+			}
+			next_env = LIST_FIRST(&env_sched_list[cur_head_index]);
+			if (next_env->env_status == ENV_RUNNABLE)
+			{
+				now_have = 1;
+				break;
+			}
+			LIST_REMOVE(next_env, env_sched_link);
+			LIST_INSERT_HEAD(&env_sched_list[!cur_head_index], next_env, env_sched_link);
+		}
+		if (!now_have)
+		{
+			while (1)
+			{
+				if (LIST_EMPTY(&env_sched_list[cur_head_index]))
+				{
+					panic("^^^^^^No env is RUNNABLE!^^^^^^");
+				}
+				next_env = LIST_FIRST(&env_sched_list[cur_head_index]);
+				if (next_env->env_status == ENV_RUNNABLE)
+				{
+					now_have = 1;
+					break;
+				}
+				LIST_REMOVE(next_env, env_sched_link);
+				LIST_INSERT_HEAD(&env_sched_list[!cur_head_index], next_env, env_sched_link);
+			}
+		}
+		LIST_REMOVE(next_env, env_sched_link);
+		LIST_INSERT_HEAD(&env_sched_list[!cur_head_index], next_env, env_sched_link);
+		cur_lasttime = next_env->env_pri;
+		env_run(next_env);
+		panic("^^^^^^sched yield jump faild^^^^^^");
+	}
+	env_run(curenv);
+	panic("^^^^^^sched yield reached end^^^^^^");
+
 }
